58baf8db0244c3c5e621f65c3e05735327fdf389
Sten Tamkivi <sten@tamkivi.com>
2025-02-18 17:20:08 +0200

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
-# remote:
-# hugo && rsync -avz --delete public/ stentamkivi@tazewell.dreamhost.com:~/sten.tamkivi.com/
+#!/bin/bash
 
-# local:
-hugo && rsync -avz --delete public/ ~/sten.tamkivi.com/
-hugo server
+# Check if we're running on the remote server (Dreamhost)
+if [[ "$(hostname)" == *"dreamhost"* ]]; then
+    echo "Running on Dreamhost server..."
+    hugo && rsync -avz --delete public/ stentamkivi@sten.tamkivi.com:~/sten.tamkivi.com/
+else
+    echo "Running locally..."
+    # hugo && rsync -avz --delete public/ ~/sten.tamkivi.com/
+    hugo server
+fi
