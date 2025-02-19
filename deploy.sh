d35af1e689c6189f2f7ff4a9cee54dcce9cd105c
Sten Tamkivi <sten@tamkivi.com>
2025-02-19 10:52:32 +0200

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+# fixed remote deployment for Dreamhost
 
-# Check if we're running on the remote server (Dreamhost)
-if [[ "$(hostname)" == *"dreamhost"* ]]; then
-    echo "Running on Dreamhost server..."
+if [[ "$1" =~ ^p(rod(uction)?)?$ ]]; then
+    echo "Running production deployment..."
     hugo && rsync -avz --delete public/ stentamkivi@sten.tamkivi.com:~/sten.tamkivi.com/
 else
-    echo "Running locally..."
-    # hugo && rsync -avz --delete public/ ~/sten.tamkivi.com/
+    echo "Running local development server..."
     hugo server
 fi
